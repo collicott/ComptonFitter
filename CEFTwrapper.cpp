1d1fe14d52fa9d1888e497f40845651a16b4159d
Cristina <cristina@yoshi.office.a2.kph>
2015-07-09 14:29:36 +0200

    CEFTwrapper: A wrapper class for Pascalutsa

---
+++ CEFTwrapper.cpp
@@ -0,0 +1,71 @@
+#include "CEFTwrapper.h"
+
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+double Fitter::Result::read_double(const string &filename) const
+{
+    const string absolute_filename(folder+"/"+filename);
+
+    ifstream input;
+
+    input.open(absolute_filename);
+
+    if(!input.is_open())
+        throw result_file_error(absolute_filename);
+
+    double dummy=0.0;
+    double value=0.0;
+    input >> dummy >> value;
+    input.close();
+    return value;
+}
+
+Fitter::Result::~Result() {
+    if(!folder.empty()) {
+//        cout << "removing " + folder << endl;
+        system(string("rm -rf "+folder).c_str());
+    }
+}
+
+Fitter::Result Fitter::Fit(const double th, const double E, const double a, const double b, const double E1E1, const double M1M1, const double E1M2, const double M1E2) {
+
+    ++n_calls;
+
+    char folder[256] = "/tmp/comptonfit_XXXXXX";
+
+    string tmp = mkdtemp(folder);
+
+    if(tmp.empty())
+        throw std::runtime_error("Can't create fitter output directory");
+
+    string cmd = command + " "
+            + to_string(th) + " " + to_string(E) + " " + to_string(a) + " " + to_string(b) + " " + to_string(E1E1) + " " + to_string(M1M1) + " " + to_string(E1M2) + " " + to_string(M1E2) + " " + tmp;
+
+  //  cout << "Running " << cmd << endl;
+    int r = system(cmd.c_str());
+
+    if(r!=0)
+        throw fit_command_error(r);
+
+    return std::move(Result(tmp));
+}
+
+
+
+double Fitter::Result::GetSigma2x() const
+{
+    return read_double("xSigma2x_Adistribution.output");
+}
+
+double Fitter::Result::GetSigma2z() const
+{
+    return read_double("xSigma2z_Adistribution.output");
+}
+
+double Fitter::Result::GetSigma3() const
+{
+    return read_double("xBa_Adistribution.output");
+}
---
+++ CEFTwrapper.h
@@ -0,0 +1,67 @@
+#ifndef CEFTWRAPPER_H
+#define CEFTWRAPPER_H
+
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <exception>
+
+class Fitter {
+private:
+
+    unsigned int n_calls = 0;
+
+    // The command to call
+    std::string command = "sh runComptonEFT";
+
+public:
+    class Result {
+        friend class Fitter;
+    protected:
+        std::string folder;
+
+        Result(const std::string& f): folder(f) {}
+
+        double read_double(const std::string& filename) const;
+
+    public:
+        Result(const Result&) = delete;
+        Result& operator= (const Result&) = delete;
+        Result(Result&& a) = default;
+
+        virtual ~Result();
+
+        double GetSigma2x() const;
+        double GetSigma2z() const;
+        double GetSigma3() const;
+    };
+
+    class result_file_error: public std::exception {
+    protected:
+        std::string filename;
+    public:
+        result_file_error(const std::string& file): filename(file) {}
+    public:
+        const char *what() const throw() { return filename.c_str(); }
+    };
+
+    class fit_command_error: public std::exception {
+    protected:
+        std::string msg;
+        int code;
+    public:
+        fit_command_error(const int c): code(c), msg("Fit command return code "+std::to_string(c)) {}
+    public:
+        const char *what() const throw() { return msg.c_str(); }
+        const int ErrorCode() const throw() { return code; }
+    };
+
+    Fitter() {}
+
+    Result Fit(const double th, const double E, const double a, const double b, const double E1E1, const double M1M1, const double E1M2, const double M1E2);
+
+    unsigned int GetNCalls() const { return n_calls; }
+
+};
+
+#endif
