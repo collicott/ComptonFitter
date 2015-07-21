92452bdf7151a8842a185eddd2c96b4b012b7224
Cristina <cristina@yoshi.office.a2.kph>
2015-07-21 21:12:41 +0200

    SP_fit update

--- src/SP_fit.cpp
+++ src/SP_fit.cpp
@@ -2,6 +2,8 @@
 #include <APLCON.hpp>
 #include "CEFTwrapper.h"
 #include "DataHandling.h"
+#include "TFile.h"
+#include "TNtuple.h"
 
 using namespace std;
 
@@ -41,32 +43,39 @@ struct constraints {
 
 int main(int argc, char *argv[])
 {
+    // Set up some ROOT storage
+    TFile *f = new TFile("SP_fit.root","RECREATE");
+    TNtuple *ntuple = new TNtuple("SP_fit",
+                                  "SP_fit",
+                                  "call:alpha:beta:E1E1:M1M1:E1M2:M1E2");
+
+    int call = 0;
+
     // Set up APLCON
     APLCON::Fit_Settings_t settings = APLCON::Fit_Settings_t::Default;
     settings.MaxIterations = 1000;
-    settings.DebugLevel = 5;
+    settings.DebugLevel = 4;
     APLCON aplcon("Polarisabilities", settings);
-
-    APLCON::Variable_Settings_t var_settings = APLCON::Variable_Settings_t::Default;
 
 	// Set up fitter
     Fitter myfit;
 
     // Set initial values
-    params fitparam(12.0, 1.9, -3.0, 3.5, -0.8, 2.0);
-    constraints alpha_beta_sum(13.8, 1.8);
-    constraints alpha_beta_diff(10, 1.8);
-    constraints gamma_0(1.01, 0.08);
+    params fitparam(12.0, 2, 0., 0., 0., 0.);
+    constraints alpha_beta_sum(13.8, 0.4);
+    constraints alpha_beta_diff(7.6, 1.7);
+    constraints gamma_0(-1.01, 0.08);
     constraints gamma_pi(8.0, 1.8);
 
     // Add params as unmeasured variables
     aplcon.AddUnmeasuredVariable("alpha",fitparam.alpha);
     aplcon.AddUnmeasuredVariable("beta", fitparam.beta);
-    aplcon.AddUnmeasuredVariable("E1E1",fitparam.E1E1,var_settings);
-    aplcon.AddUnmeasuredVariable("M1M1",fitparam.M1M1,var_settings);
-    aplcon.AddUnmeasuredVariable("E1M2",fitparam.E1M2,var_settings);
-    aplcon.AddUnmeasuredVariable("M1E2",fitparam.M1E2,var_settings);
+    aplcon.AddUnmeasuredVariable("E1E1",fitparam.E1E1);
+    aplcon.AddUnmeasuredVariable("M1M1",fitparam.M1M1);
+    aplcon.AddUnmeasuredVariable("E1M2",fitparam.E1M2);
+    aplcon.AddUnmeasuredVariable("M1E2",fitparam.M1E2);
 
+    // Add some measured constraints
     aplcon.AddMeasuredVariable("alpha_beta_sum",alpha_beta_sum.value, alpha_beta_sum.error);
     aplcon.AddMeasuredVariable("alpha_beta_diff",alpha_beta_diff.value, alpha_beta_diff.error);
     aplcon.AddMeasuredVariable("gamma_0",gamma_0.value, gamma_0.error);
@@ -90,10 +99,9 @@ int main(int argc, char *argv[])
     aplcon.AddConstraint("gamma0", {"E1E1", "M1M1", "E1M2", "M1E2", "gamma_0"},  g0_constraint);
     aplcon.AddConstraint("gammapi",{"E1E1", "M1M1", "E1M2", "M1E2", "gamma_pi"}, gpi_constraint);
 
-
     // Set up Data handler and add data points as measured variables
     DataHandling dataHandler;
-    string filelist(argv[1]);
+//    string filelist(argv[1]);
     dataHandler.process_file_list(argv[1]);
     vector<data> datapoint = dataHandler.GetDataList();
 
@@ -116,7 +124,7 @@ int main(int argc, char *argv[])
         pointname << "datapoint" << i;
 
         // setup a lambda function which returns 0
-        auto equality_constraint = [&myfit,&datapoint,i]
+        auto equality_constraint = [&myfit,&datapoint,i,&ntuple,&call]
                 (double experiment,
                 double alpha,
                 double beta,
@@ -133,14 +141,31 @@ int main(int argc, char *argv[])
                                     M1M1,
                                     E1M2,
                                     M1E2);
-//            cout << alpha<< " "
-//               << beta<<" "
-//               << E1E1<<" "
-//               << M1M1 << " "
-//               << E1M2 << " "
-//               << M1E2 << endl;
 
-            return experiment - theory.GetSigma3();
+            ntuple->Fill(call,
+                         alpha,
+                         beta,
+                         E1E1,
+                         M1M1,
+                         E1M2,
+                         M1E2);
+            call++;
+
+
+            if (datapoint[i].data_type == Sigma_3)
+                return experiment - theory.GetSigma3();
+
+            else if (datapoint[i].data_type == Sigma_2x)
+                return experiment - theory.GetSigma2x();
+
+            else if (datapoint[i].data_type == Sigma_2z)
+                return experiment - theory.GetSigma2z();
+
+            else if (datapoint[i].data_type == Cross)
+                return experiment - theory.GetCross();
+
+            else cout << "ERROOOROROROROROROR" << endl;
+            return 1000000.0;
         };
 
         aplcon.AddConstraint(
@@ -155,45 +180,13 @@ int main(int argc, char *argv[])
                      equality_constraint);
     }
 
-    // setup a lambda function which returns 0
-    auto alpha_constraint = [] (double alpha, double alpha_theory)
-    {
-        return alpha - alpha_theory;
-    };
-
-
     // do the fit, obtain ra structure
     const APLCON::Result_t& ra = aplcon.DoFit();
     cout << ra << endl;
 
     cout << "Calls to Executable: " << myfit.GetNCalls() << endl;
 
+    f->Write();
+
     return 0;
 }
-
-/*
-int main()
-{
-   // Set up fitter
-   Fitter myfit;
-   params fitparam(12.0, 1.9, -4.5, 3.0, -0.8, 2.0);
-
-   for (double E1E1 = -5.5; E1E1 < 5.5; E1E1+=0.25)
-   {
-       auto theory = myfit.Fit(90,
-                               287,
-                               fitparam.alpha,
-                               fitparam.beta,
-                               E1E1,
-                               fitparam.M1M1,
-                               fitparam.E1M2,
-                               fitparam.M1E2);
-
-       cout << E1E1 << " " << theory.GetSigma2x() << endl;
-
-   }
-
-   return 0;
-
-}
-*/
