3a3eac284d8f6e923586f561157435e418b12503
Cristina <cristina@yoshi.office.a2.kph>
2015-08-04 12:10:44 +0200

    Phil_fit: Added Pasquini

--- src/phil_fit.cpp
+++ src/phil_fit.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <APLCON.hpp>
 #include "CEFTwrapper.h"
+#include "DISPwrapper.h"
 #include "DataHandling.h"
+#include "DisplayResults.h"
 #include "TFile.h"
 #include "TNtuple.h"
 #include <ctime>
+#include <string>
 
 using namespace std;
 
@@ -48,6 +51,27 @@ int main(int argc, char *argv[])
     // Set up some time
     clock_t begin = clock();
 
+    // Quickly parse through arguments to find theory and filelist
+    string theory_code;
+    string filelist;
+
+    string flag(argv[2]);
+    if (flag == "--Pascalutsa") theory_code = "Pascalutsa";
+    else if (flag == "--Pasquini") theory_code = "Pasquini";
+    else {
+        cout << "Error: unknown theory " << argv[2] << endl;
+        return 1;
+    }
+
+    // Set up Data handler
+    DataHandling dataHandler;
+    dataHandler.process_file_list(argv[1]);
+    vector<data> datapoint = dataHandler.GetDataList();
+
+    // Set up fitters
+    CEFTwrapper CEFTfit;
+    DISPwrapper DISPfit;
+
     // Set up some ROOT storage
     TFile *f = new TFile("phil_fit.root","RECREATE");
     TNtuple *ntuple = new TNtuple("phil_fit",
@@ -63,28 +87,18 @@ int main(int argc, char *argv[])
     settings.DebugLevel = 5;
     APLCON aplcon("Polarisabilities", settings);
 
-     // Set up fitter
-    Fitter myfit;
-
     // Set initial values
 
     // old alpha-beta
-//      params fitparam(12.1, 1.6, -3.3, 3.0, 0.2, 1.1);     // (nominal BxPT) -3.7
 //    params fitparam(12.1, 1.6, -4.3, 2.9, -0.02, 2.2);   // (nominal HDPV) -3.7
+//    params fitparam(12.1, 1.6, -3.3, 3.0, 0.2, 1.1);     // (nominal BxPT) -3.7
 
     // new alpha-beta
-//    params fitparam(11.2, 2.5, -3.3, 3.0, 0.2, 1.1);     // (nominal BxPT) -3.7
-    params fitparam(11.2, 2.5, -4.3, 2.9, -0.02, 2.2);    // (nominal HDPV)   -3.6
+//      params fitparam(11.2, 2.5, -4.3, 2.9, -0.02, 2.2);    // (nominal HDPV)   -3.6
+    params fitparam(11.2, 2.5, -3.3, 3.0, 0.2, 1.1);     // (nominal BxPT) -3.7
 
     // Add params as unmeasured variables
     aplcon.AddUnmeasuredVariable("E1E1",fitparam.E1E1);
-
-    // Set up Data handler
-    DataHandling dataHandler;
-
-    string filelist(argv[1]);
-    dataHandler.process_file_list(filelist);
-    vector<data> datapoint = dataHandler.GetDataList();
 
     // Add data points as measured variables
     for (auto i = 0; i < datapoint.size(); i++)
@@ -106,20 +120,31 @@ int main(int argc, char *argv[])
         pointname << "datapoint" << i;
 
         // setup a lambda function which returns 0
-        auto equality_constraint = [&myfit,&fitparam,&datapoint,i,&ntuple,&call]
+        auto equality_constraint = [&CEFTfit,&DISPfit,&fitparam,&datapoint,i,&ntuple,&call,&theory_code]
                 (double experiment,
                  double E1E1)
         {
-            auto theory = myfit.Fit(datapoint[i].theta,
-                                    datapoint[i].energy,
-                                    fitparam.alpha,
-                                    fitparam.beta,
-                                    E1E1,
-                                    fitparam.M1M1,
-                                    fitparam.E1M2,
-                                    fitparam.M1E2,
-                                    datapoint[i].data_type);
-
+            double theory = 0;
+            if(theory_code == "Pascalutsa")
+                 theory = CEFTfit.Fit(datapoint[i].theta,
+                                      datapoint[i].energy,
+                                      fitparam.alpha,
+                                      fitparam.beta,
+                                      E1E1,
+                                      fitparam.M1M1,
+                                      fitparam.E1M2,
+                                      fitparam.M1E2,
+                                      datapoint[i].data_type);
+            else if (theory_code == "Pasquini")
+                 theory = DISPfit.Fit(datapoint[i].theta,
+                                      datapoint[i].energy,
+                                      fitparam.alpha,
+                                      fitparam.beta,
+                                      E1E1,
+                                      fitparam.M1M1,
+                                      fitparam.E1M2,
+                                      fitparam.M1E2,
+                                      datapoint[i].data_type);
             ntuple->Fill(call,
                          fitparam.alpha,
                          fitparam.beta,
@@ -142,10 +167,18 @@ int main(int argc, char *argv[])
     const APLCON::Result_t& ra = aplcon.DoFit();
     cout << ra << endl;
 
-    cout << "Calls to Executable: " << myfit.GetNCalls() << endl;
-    cout << "Number of re-used folders: " << myfit.GetNFolders() << endl;
-
-    f->Write();
+    if (CEFTfit.GetNCalls() != 0)
+    {
+        cout << "Using CEFT:" << endl;
+        cout << "Calls to Executable: " << CEFTfit.GetNCalls() << endl;
+        cout << "Number of re-used folders: " << CEFTfit.GetNFolders() << endl;
+    }
+    else if (DISPfit.GetNCalls() != 0)
+    {
+        cout << "Using DISP:" << endl;
+        cout << "Calls to Executable: " << DISPfit.GetNCalls() << endl;
+        cout << "Number of re-used folders: " << DISPfit.GetNFolders() << endl;
+    }
 
     // output time performance
     clock_t end = clock();
@@ -153,6 +186,19 @@ int main(int argc, char *argv[])
 
     cout << "*************************" << endl;
     cout << "Fit completed in " << elapsed_secs << " seconds." << endl;
+    cout << "E1E1 is found to be: " << ra.Variables.at("E1E1").Value.After << " +- " << ra.Variables.at("E1E1").Sigma.After << endl;
+
+
+    // Let's display the results
+    DisplayResults display;
+    display.ShowFitResults(datapoint,
+                           fitparam.alpha,
+                           fitparam.beta,
+                           ra.Variables.at("E1E1").Value.After,
+                           fitparam.M1M1,
+                           fitparam.E1M2,
+                           fitparam.M1E2);
+    f->Write();
 
     return 0;
 }
